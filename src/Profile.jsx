d4bc02a55de2404fb05f0a9c56fcac6bd140a6fe
cs-joey <89863700+cs-joey@users.noreply.github.com>
2024-02-25 22:03:00 -0600

    Update Profile.jsx

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,42 +6,36 @@ import { supabase } from './supabaseClient'; // Import your Supabase client
 
 export default function Profile({ session }) {
 
-    // const Profile = ({ profiles }) => {
-    // const [followers, setFollowers] = useState(0);
-    // const [following, setFollowing] = useState(0);
+    const [followers, setFollowers] = useState(0);
+    const [following, setFollowing] = useState(0);
 
-    let followers = 0;
-    let following = 0;
     useEffect(() => {
-        let ignore = false
+        let ignore = false;
         async function getProfile() {
-            // setLoading(true)
-            const { user } = session
+            const { user } = session;
 
             const { data, error } = await supabase
                 .from('profiles')
-                .select(`followingCount, followersCount`)
+                .select(`followingCount, followerCount`)
                 .eq('id', user.id)
-                .single()
+                .single();
 
             if (!ignore) {
                 if (error) {
-                    console.warn(error)
+                    console.warn(error);
                 } else if (data) {
-                    setUsername(data.username)
-                    setAvatarUrl(data.avatar_url)
+                    setFollowers(data.followerCount);
+                    setFollowing(data.followingCount);
                 }
             }
-
-            // setLoading(false)
         }
 
-        getProfile()
+        getProfile();
 
         return () => {
-            ignore = true
+            ignore = true;
         }
-    }, [session])
+    }, [session]);
 
     return (
         <div className="app-container">
