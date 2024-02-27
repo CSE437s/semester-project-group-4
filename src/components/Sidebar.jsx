e52557f3e987862fbfee31c0d5a213c6f26ef9b6
cs-joey <89863700+cs-joey@users.noreply.github.com>
2024-02-27 01:03:39 -0600

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,16 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/Sidebar.css'
 
 function Sidebar() {
-    return (
-        <div className="sidebarMain border-right" id="sidebar-wrapper">
-          <div className="sidebarName sidebar-heading">groove</div>
-          <div className="sideBarPages">
-            <a href="../Profile" className="sideBarPage list-group-item list-group-item-action">Profile</a>
-            <a href="../Share" className="sideBarPage list-group-item list-group-item-action">Share</a>
-            <a href="../Feed" className="sideBarPage list-group-item list-group-item-action">Feed</a>
-          </div>
-        </div>
-      );
+  return (
+    <div className="sidebarMain border-right" id="sidebar-wrapper">
+      <div className="sidebarName sidebar-heading">groove</div>
+      <div className="sideBarPages">
+
+        <Link to="/Profile" className="sideBarPage list-group-item list-group-item-action">Profile</Link>
+        <Link to="/Share" className="sideBarPage list-group-item list-group-item-action">Share</Link>
+        <Link to="/Feed" className="sideBarPage list-group-item list-group-item-action">Feed</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar
