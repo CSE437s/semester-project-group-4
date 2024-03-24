9539f101d39a663e8aa60fc8f99172ba3f718edb
JayWon01 <jaywon@wustl.edu>
2024-03-24 15:53:27 -0500

    minor changes in profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -10,22 +10,13 @@ export default function Profile({ session }) {
     const [friendCount, setFriendCount] = useState(0);
     const [username, setUsername] = useState('');
     
-    // const axios = require('axios');
-    // const qs = require('qs');
-
-    //const client_id = process.env.REACT_APP_SPOTIFY_API_ID; // Your client id
-    //const client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET; // Your secret
-    //const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64');
-
-
-    let client_id = "1892c29e22e44ec686fa22a8e891b0f9";
-    let redirect = "http://localhost:5173/Share"; //takes us back here after agreeing to Spotify
-    // let redirect = "https://semester-project-group-4.vercel.app/Share" //use for vercel host
     const clientId = "1892c29e22e44ec686fa22a8e891b0f9";
-    const redirectUri = "http://localhost:5173/Share";
-    const scope = 'user-read-private user-read-email user-top-read';
+    //const clientId = process.env.REACT_APP_SPOTIFY_API_ID; // Your client id
 
-    const AUTHORIZE = "https://accounts.spotify.com/authorize";
+    const redirectUri = "http://localhost:5173/Share"; //takes us back here after agreeing to Spotify
+    // let redirectUri = "https://semester-project-group-4.vercel.app/Share" //use for vercel host
+
+    const scope = 'user-read-private user-read-email user-top-read';
 
     // useEffect(() => {
     //     let ignore = false;
@@ -89,78 +80,6 @@ export default function Profile({ session }) {
         await redirectToSpotifyAuthorize();
     }
 
-    async function authorize() {
-        const generateRandomString = (length) => {
-            const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            const values = crypto.getRandomValues(new Uint8Array(length));
-            return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-        };
-        
-        const codeVerifier  = generateRandomString(64);
-        console.log("in profile verif: " + codeVerifier);
-        
-        const sha256 = async (plain) => {
-        const encoder = new TextEncoder()
-        const data = encoder.encode(plain)
-        return window.crypto.subtle.digest('SHA-256', data)
-        };
-        
-        const base64encode = (input) => {
-        return btoa(String.fromCharCode(...new Uint8Array(input)))
-            .replace(/=/g, '')
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_');
-        };
-    
-        const hashed = await sha256(codeVerifier);
-        const codeChallenge = base64encode(hashed);
-        
-        const scope = 'user-read-private user-read-email user-top-read';
-        const authUrl = new URL("https://accounts.spotify.com/authorize")
-        
-        // generated in the previous step
-        window.localStorage.setItem('code_verifier', codeVerifier);
-        
-        const params =  {
-        response_type: 'code',
-        client_id: clientId,
-        scope,
-        code_challenge_method: 'S256',
-        code_challenge: codeChallenge,
-        redirect_uri: redirectUri,
-        }
-        
-        authUrl.search = new URLSearchParams(params).toString();
-        window.location.href = authUrl.toString();
-
-
-
-
-
-        // let redirect_uri = "http://localhost:8000/callback";
-
-        // let url = AUTHORIZE;
-        // url += "?client_id=" + client_id;
-        // url += "&response_type=code";
-        // url += "&redirect_uri=" + encodeURI(redirect);
-        // url += "&show_dialog=true";
-        // url += "&scope=user-read-private user-read-email user-read-playback-state user-top-read";
-        // window.location.href = url;
-        
-        // const response = await axios.get('http://localhost:8000/login');
-        // console.log(response.data);
-        // window.location.href = response;
-
-        // axios.get('http://localhost:8080/login')
-        // .then(response => {
-        //     // Handle response
-        //     console.log(response);
-        // })
-        // .catch(error => {
-        //     // Handle error
-        //     console.error(error);
-        // });
-    }
 
     const handleAddFriend = async () => {
         //find other users uuid
