cdb1a732faac27c8ef6852b2ae6d10777297fa2d
samyoo22 <112423814+samyoo22@users.noreply.github.com>
2024-03-26 18:02:19 -0500

    Auth.jsx updated
    Auth.jsx updated

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,4 +1,4 @@
-import './css/Account.css';
+//import './css/Account.css';
 import { useState, useEffect } from 'react';
 import { supabase } from './supabaseClient';
 import { useNavigate } from 'react-router-dom';
