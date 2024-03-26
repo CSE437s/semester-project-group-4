53603b73777e27d0a544c3ace9e9c607ebe6612e
cs-joey <89863700+cs-joey@users.noreply.github.com>
2024-03-26 15:20:40 -0500

    Update SongLayout.jsx

--- src/components/SongLayout.jsx
+++ src/components/SongLayout.jsx
@@ -19,7 +19,7 @@ const SongLayout = (props) => { //props.songs
         const { data, error } = await supabase
             .from('shared_songs')
             .insert([
-                { id: session.user.id, song: json, spotifySongId: json.song.id }
+                { id: session.user.id, song: json, spotifySongId: json.id }
             ]);
 
         if (error) {
