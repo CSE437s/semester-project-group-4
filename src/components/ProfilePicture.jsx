255e30387c03f625a8924a466a1f2f108628c084
cs-joey <89863700+cs-joey@users.noreply.github.com>
2024-04-19 18:03:25 -0500

    improved profile pic upload for new account

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
+import '../index.css'
 
 const ProfilePicture = () => {
     const [profilePic, setProfilePic] = useState(null);
@@ -127,9 +128,26 @@ const ProfilePicture = () => {
             {profilePic ? (
                 <img src={profilePic} onClick={() => document.getElementById('fileInput').click()} />
             ) : (
-                <button onClick={() => document.getElementById('fileInput').click()}>Upload Image</button>
+                // <button onClick={() => document.getElementById('fileInput').click()}>Upload Image</button>
+                <div className="mb-3 w-96">
+                    <label
+                        htmlFor="fileInput"
+                        className="mb-2 inline-block text-neutral-700"
+                    >
+                        Upload a Profile Pic
+                    </label>
+                    <input
+                        className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none"
+                        type="file"
+                        id="fileInput"
+                        onChange={handleFileUpload}
+                    />
+                </div>
+
+
+
             )}
-            <input id="fileInput" type="file" style={{ display: 'none' }} onChange={handleFileUpload} />
+            {/* <input id="fileInput" type="file" style={{ display: 'none' }} onChange={handleFileUpload} /> */}
         </div>
     );
 };
@@ -137,7 +155,7 @@ const ProfilePicture = () => {
 export default ProfilePicture;
 
 
-
+//placeholder='Upload a Profile Pic'
 /*
 
 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
