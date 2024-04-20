dac302a7e5990e6d4275324f9983efc04b235c81
cs-joey <89863700+cs-joey@users.noreply.github.com>
2024-04-20 14:11:55 -0500

    added onboarding page, improve UI friends

---
+++ src/Onboarding.jsx
@@ -0,0 +1,16 @@
+import { supabase } from './supabaseClient';
+import React, { useState, useEffect, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function Onboarding({ session }) {
+
+
+
+    return(
+        <div>
+            <p>Skip</p>
+        </div>
+
+    );
+
+};
